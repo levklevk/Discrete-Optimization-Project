4e4125c9975782fc24fe33b7b757bf913ae66955
Kazakovtsev Lev <levk@bk.ru>
2012-02-29 17:07:06 +0100

    jkhjkhk

--- foundry.py
+++ foundry.py
@@ -26,20 +26,56 @@ class foundry:
                             49,48,40,43,41,45,49,45,40,49,\
                             40,40,50,45,45,44],dtype=int)
         ''' Vilumes of orders'''
-        self.W=numpy.array([20,25000,30,450,45,145,450,4500,4000,400,\
-			    22241,12241,940,745,445,20470,12350,145,540,550,\
+        self.W=numpy.array([20,2501,30,450,45,145,450,4500,4000,400,\
+			    2241,12241,940,745,445,2047,1231,145,540,550,\
                             5449,148,140,3543,6441,5545,2249,345,440,549,\
-                            140,140,150,145,4598,12000],dtype=int)
+                            140,140,150,145,451,1201],dtype=int)
         '''Ability of the FB's to produce kinds of production'''
         self.z=numpy.array([[1,1,1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1,1,1, 1,1,1,1,1,1], \
     			    [1,1,1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1,1,1, 0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0], \
     			    [1,1,1,1,1,1,1,1,1,1, 0,0,0,0,0,0,0,0,0,0, 1,1,1,1,1,1,1,1,1,1, 1,1,1,0,0,0]  \
     			    ],dtype=int)
+	self.notz=1-self.z
 	''' Terms of finishing production'''
 	self.T=numpy.array([31,31,31,31,31,31,31,31,31,31,\
-			    31,31,31,15,15,15,15, 5, 5,15,\
+			    31,31,31,15,15,15,15,20,20,15,\
                             10,10,10,31,31,31,31,31,31,31,\
                             31,31,31,31,31,31],dtype=int)
+        self.WIalpha=int(self.alpha*self.W.sum()/self.I)
+        
+        self.maxConstraintsViolation=self.WIalpha*self.I*self.W.sum()
+
+	'''l lumbers corresponding to the elements of the vector Y'''
+        IJKmap=numpy.zeros((self.I,self.J,self.Kmax,self.L,3),dtype=int)
+        '''Maps actual boolean variables to the matrix'''
+        self.booleanMap=numpy.zeros((self.I,self.J,self.Kmax,self.L),dtype=bool)
+        for i in range(0,self.I):
+    	    for j in range(0,self.J):
+    		for k in range(0,self.K[j]):
+    		    for l in range(0,self.L):
+    			self.booleanMap[i,j,k,l]=self.z[j,l]
+    			IJKmap[i,j,k,l,0]=i
+    			IJKmap[i,j,k,l,1]=j
+    			IJKmap[i,j,k,l,2]=k
+    	'''		self.booleanMap[i,j,k,l]=True'''
+    	self.booleanMap=self.booleanMap.reshape(self.I*self.J*self.Kmax*self.L)
+        IJKmap=IJKmap.reshape(self.I*self.J*self.Kmax*self.L,3)[self.booleanMap,:]
+        '''length of boolean vector to operate with '''
+        self.booleanLen=self.booleanMap.sum()
+        '''array of indexes corresponding to the first and last position in the boolean vector  for matrix Y'''
+        self.IJKstart=numpy.zeros((self.I,self.J,self.Kmax),dtype=int)
+        self.IJKstart.fill(-1)
+        self.IJKstops=numpy.zeros((self.I,self.J,self.Kmax),dtype=int)
+        self.IJKstops.fill(-1)
+        for ii in range(0,self.booleanLen):
+    	    (i,j,k)=IJKmap[ii]
+    	    if self.IJKstart[i,j,k]==-1:
+    		self.IJKstart[i,j,k]=ii
+	    self.IJKstops[i,j,k]=ii+1
+        self.IJKstart=filter(lambda x:x>=0,self.IJKstart.reshape(self.I*self.J*self.Kmax))
+        self.IJKstops=filter(lambda x:x>=0,self.IJKstops.reshape(self.I*self.J*self.Kmax))
+        self.Ncuts=len(self.IJKstart)
+        
         
     def C(self,Y):
 	'''Y is a  boolean 4D matrix (see the Antamoshkin's paper)'''
@@ -66,7 +102,8 @@ class foundry:
 	penal=0.0
 	'''1st, we try to count A2 penalties'''
 	'''for the 1st i=0, it is just multiplication by 2'''
-	A2_arr=numpy.zeros(self.I,dtype=int)
+	A2_arr=numpy.zeros(self.I,dtype=float)
+	WIalfaf=float(self.WIalpha)
 	for i in range(0,self.I):
 	    A2=0
 	    for j in range(0,self.J):
@@ -77,18 +114,55 @@ class foundry:
 			i1=i-1
 			'''print "i=",i,"j=",j,"k=",k,"Y=",Y1[i1,j,k]*Y2[i1,j,k]'''
 			A2+=(YProd[i1,j,k]*self.V).sum()
-	    print "A2 ",i," = ",A2
-	    A2_arr[i]=A2
-	A1_arr=numpy.zeros(self.L,dtype=int)
+	    A2_arr[i]=0. if A2>self.WIalpha else float(self.WIalpha-A2)/WIalfaf
+
+	A1_arr=numpy.zeros(self.L,dtype=float)
 	for l in range(0,self.L):
-	    A1=0
+	    A1=(Y[0,:,:,l]*2*self.V[l]).sum()+\
+		    (YProd[:self.T[l]-1,:,:,l]*self.V[l]).sum()
+	    A1_arr[l]=0. if A1>self.W[l] else float(self.W[l]-A1)/float(self.W[l])
 	    
+	'''Additional constraint'''
+	z_nevyp=0
+	for i in range(0,self.I):
+	    for k in range(0,self.Kmax):
+		z_nevyp+=(Y[i,:,k,:]*self.notz).sum()
+	'''By now, A1 and A2 are calculated and we have to calculate the penalty itself'''
+	'''Additional constraint sum(Y)<=1, constraint 9 in the paper'''
+	sumYpenalty=0
+	for i in range(0,self.I):
+	    for j in range(0,self.J):
+		for k in range(0,self.K[j]):
+		    sumY=Y[i,j,k].sum()
+		    sumYpenalty+=0 if sumY<=1 else sumY-1
+	assert z_nevyp==0 
+	assert sumYpenalty==0
+	return A1_arr.sum()+A2_arr.sum()*0.+float(z_nevyp)/float(self.booleanLen)+sumYpenalty
 		
 	
+    def C_and_penalty(self,X):
+	Y=self.scalar_X_transcode(X)
+	return (self.C(Y),self.penalty(Y))
+    
     def test_penalty(self):
 	y=numpy.ones((self.I,self.J,self.Kmax,self.L),dtype=bool)
 	for i in range(3,15):
 	    y[i]=numpy.zeros((self.J,self.Kmax,self.L),dtype=bool)
 	self.penalty(y)
     
+    def scalar_X_transcode(self,X):
+	'''based on vector X of boolean variables generates and returns matrix Y'''
+	YY=numpy.zeros(self.I*self.J*self.Kmax*self.L,dtype=bool)
+	YY[self.booleanMap]=X
+	return YY.reshape(self.I,self.J,self.Kmax,self.L)
+	
+    def test_scalar_transcode(self):
+	X=numpy.ones(self.booleanLen,dtype=bool)
+	Y=self.scalar_X_transcode(X)
+	assert Y.sum()==self.booleanLen
+	X=numpy.zeros(self.booleanLen,dtype=bool)
+	Y=self.scalar_X_transcode(X)
+	assert Y.sum()==0
+	
+	
 	
